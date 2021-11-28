9e5b1fd345e04a8165bec7353279aa444b925849
vamsisanthosh <93900819+vamsisanthosh@users.noreply.github.com>
2021-11-28 16:30:48 +0530

    Add files via upload

---
+++ PSB_1483/sq. root of a number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	double num, sr;
+	
+	printf("Enter any number of your choice: ");
+	scanf("%lf",&num);
+	
+	sr = sqrt(num);
+	
+	printf("The square root of your number is: %.2lf",sR);
+	
+	return 0;
+}
